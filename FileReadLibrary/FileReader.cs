04f6e9171b88156f89166246af0eac6bbe80f0a1
trydems <adem.bacaj@hotmail.com>
2023-10-25 14:23:22 +0200

    Version 1

---
+++ FileReadLibrary/FileReader.cs
@@ -0,0 +1,28 @@
+﻿namespace FileReadLibrary;
+using System.IO;
+
+public class FileReader
+{
+    public string ReadTextFile(string filePath)
+    {
+        try
+        {
+            // Check if the file exists
+            if (File.Exists(filePath))
+            {
+                // Read the contents of the file
+                string content = File.ReadAllText(filePath);
+                return content;
+            }
+            else
+            {
+                return "File not found.";
+            }
+        }
+        catch (Exception ex)
+        {
+            return $"An error occurred while reading the text file: {ex.Message}";
+        }
+    }
+}
+
